b8e148f100370c7886674fcd5e32c69f79934d24
Szymon Kluka <szymonkluka@onet.pl>
2023-05-13 17:13:42 +0200

    CreateProductDTO

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, Delete, Body, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from 'src/db';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { CreateProductDTO } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -11,16 +13,16 @@ export class ProductsController {
     return this.productsService.getAll();
   }
   @Get('/:id')
-  public getById(@Param('id') id: string) {
+  public getById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.productsService.getById(id);
   }
   @Delete('/:id')
-  public deleteById(@Param('id') id: string) {
+  public deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
     this.productsService.deleteById(id);
     return { success: true }
   }
   @Post('/')
-  public create(@Body() productData: Omit<Product, 'id'>) {
+  public create(@Body() productData: CreateProductDTO) {
     return this.productsService.create(productData);
   }
 
